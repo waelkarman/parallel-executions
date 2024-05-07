5d35b93140b4bb9db2095b6a70662683bbac9fdf
Wael Karman <wael.karman@gmail.com>
2024-05-07 22:25:25 +0200

    correct exercise 5

--- OpenMP/snippet5-mandel.c
+++ OpenMP/snippet5-mandel.c
@@ -20,8 +20,6 @@
 # define NPOINTS 1000
 # define MAXITER 1000
 
-void testpoint(void);
-
 struct d_complex{
    double r;
    double i;
@@ -30,34 +28,39 @@ struct d_complex{
 struct d_complex c;
 int numoutside = 0;
 
+
+void testpoint( struct d_complex c);
+
+
 int main(){
-   int i, j;
-   double area, error, eps  = 1.0e-5;
+  int i, j;
+  double area, error, eps  = 1.0e-5;
 
 
-//   Loop over grid of points in the complex plane which contains the Mandelbrot set,
-//   testing each point to see whether it is inside or outside the set.
+  //   Loop over grid of points in the complex plane which contains the Mandelbrot set,
+  //   testing each point to see whether it is inside or outside the set.
 
-#pragma omp parallel for default(shared) private(c,eps)
-   for (i=0; i<NPOINTS; i++) {
-     for (j=0; j<NPOINTS; j++) {
-       c.r = -2.0+2.5*(double)(i)/(double)(NPOINTS)+eps;
-       c.i = 1.125*(double)(j)/(double)(NPOINTS)+eps;
-       testpoint();
-     }
-   }
+  #pragma omp parallel for default(shared) private(c) 
+    for (i=0; i<NPOINTS; i++) {
+      #pragma omp parallel for default(shared) private(c) 
+      for (j=0; j<NPOINTS; j++) {
+        c.r = -2.0+2.5*(double)(i)/(double)(NPOINTS)+eps;
+        c.i = 1.125*(double)(j)/(double)(NPOINTS)+eps;
+        testpoint(c);
+      }
+    }
 
-// Calculate area of set and error estimate and output the results
-   
-area=2.0*2.5*1.125*(double)(NPOINTS*NPOINTS-numoutside)/(double)(NPOINTS*NPOINTS);
-   error=area/(double)NPOINTS;
+  // Calculate area of set and error estimate and output the results
+  printf("numoutside value: %d\n",numoutside);
+  area=2.0*2.5*1.125*(double)(NPOINTS*NPOINTS-numoutside)/(double)(NPOINTS*NPOINTS);
+  error=area/(double)NPOINTS;
 
-   printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n",area,error);
-   printf("Correct answer should be around 1.510659\n");
+  printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n",area,error);
+  printf("Correct answer should be around 1.510659\n");
 
 }
 
-void testpoint(void){
+void testpoint(struct d_complex c){
 
 // Does the iteration z=z*z+c, until |z| > 2 when point is known to be outside set
 // If loop count reaches MAXITER, point is considered to be inside the set
